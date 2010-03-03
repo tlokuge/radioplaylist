7c6bd3c9d6b2a097e16b2c448bd4baf94668a7c9
dragon.thavisha <dragon.thavisha@853825bc-0d22-11df-bbbe-ede73d0abfb9>
2010-03-03 22:26:49 +0000

    * Modified PlayList to take in a File parameter when saving/loading. * Added user saving.

--- trunk/radioplaylist/User.java
+++ trunk/radioplaylist/User.java
@@ -1,16 +1,21 @@
 package radioplaylist;
 
+import java.util.ArrayList;
+
 public class User
 {
-    private String name;
+    private String realname;
     private String username;
     private String password;
     private PlayList library;
+    private ArrayList<PlayList> playlists;
 
-    public User(String username, String password)
+    public User(String realname, String username, String password)
     {
-        this.username = username;
-        this.password = password;
+        this.realname = realname;
+        this.username  = username;
+        this.password  = password;
+        playlists = new ArrayList<PlayList>();
     }
 
     public void setSongLibrary(PlayList library)
@@ -18,10 +23,19 @@ public class User
         this.library = library;
     }
 
+    public void addPlayList(PlayList pl)
+    {
+        if(pl != null)
+            playlists.add(pl);
+    }
+
     public boolean comparePassword(String other_password)
     {
         return password.equals(other_password);
     }
 
-    public String getUserName() { return username; }
+    public String getRealName()  { return realname; }
+    public String getUserName()  { return username; }
+    public PlayList getLibrary() { return library;  }
+    public ArrayList<PlayList> getPlayLists() { return playlists; }
 }
